cfe1425f84b645b75a7dc05a9dd663e0457bb18c
Craig <dev.etcraig@gmail.com>
2023-01-14 22:07:39 -0700

    Launches Services Built Out

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 async function startServer() {
   await loadPlanetsData();
-
+  
   server.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
   });
